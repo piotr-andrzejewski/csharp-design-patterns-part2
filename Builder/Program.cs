ec8bc1d38dcffdf9d2e7fa771fa4294ff1a8bd3d
Piotr Andrzejewski <acax.piotrandrzejewski@gmail.com>
2025-10-06 11:26:03 +0200

    implement simple builder design pattern

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace Builder;
+﻿using System.Threading.Tasks;
+
+namespace Builder;
 
 class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello");
+        var uriBuilder = new UriBuilder("https://linkedin.com/learning");
+        Console.WriteLine(uriBuilder.Host);
+        Console.WriteLine(uriBuilder.Path);
+        Console.WriteLine(uriBuilder.Port);
+
+        var client = new HttpClient();
+        var resp = await client.GetAsync(uriBuilder.Uri);
+
+        Console.WriteLine(resp.StatusCode);
+
+        uriBuilder.Path = "learning/blazor-getting-started";
+        resp = await client.GetAsync(uriBuilder.Uri);
+
+        Console.WriteLine(resp.StatusCode);
     }
 }
